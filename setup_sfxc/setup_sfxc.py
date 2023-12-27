e4db04d507f9f5c732457db1bac3538d046decd6
jradcliffe5 <jack.radcliffe@manchester.ac.uk>
2023-12-27 11:39:27 +0300

    test

--- setup_sfxc/setup_sfxc.py
+++ setup_sfxc/setup_sfxc.py
@@ -112,7 +112,7 @@ if ast.literal_eval(inputs['parallelise_scans']) == True:
 			sub_ctrl = ctrl_file.copy()
 			#rmdirs(["%s/%s%s"%(o_dir,cs,scan_c)])
 			if len(vexfile['SCHED'][scan_c]['source']) > 10:
-				srcs = ", ".join(vexfile['SCHED'][scan_c]['source'][0:10]) + ' (%d sources)'%len(vexfile['SCHED'][scan_c]['source'])
+				srcs = ", ".join(vexfile['SCHED'][scan_c]['source'][0:10]) + '+ ... (total: %d sources)'%len(vexfile['SCHED'][scan_c]['source'])
 			else:
 				srcs = ", ".join(vexfile['SCHED'][scan_c]['source'])
 			print('Making the following correlator scans: %s for sources: %s'%(scan_c,srcs))
@@ -177,10 +177,13 @@ if ast.literal_eval(inputs['parallelise_scans']) == True:
 		commands.append('rm chex.*')
 		write_job(step='run_sfxc',commands=commands,job_manager='bash',write='w')
 		commands = []
+		print('Building script for conversion to measurement sets')
 		for i in list(corr_files.keys()):
+			print('Scan %s .. done'%i)
 			commands.append('%s %s -o %s/%s.ms &'%(ast.literal_eval(inputs["j2ms2_exec"])," ".join(corr_files[i]),o_dir,i))
 		write_job(step='run_j2ms2',commands=commands,job_manager='bash',write='w')
-		commands = ['parallel -eta -j 40 %s sfxc_helperscripts/post_processing/flag_weights.py {} %.3f ::: %s*.ms'%(inputs['casa_exec'],ast.literal_eval(inputs['flag_threshold']),ctrl_file["exper_name"])]
+		print('Building script for flagging of low correlator weights')
+		commands = ['parallel -eta -j 40 %s sfxc_helperscripts/post_processing/flag_weights.py {} %.3f ::: *.ms'%(inputs['casa_exec'],ast.literal_eval(inputs['flag_threshold']))]
 		write_job(step='run_flag_data',commands=commands,job_manager='bash',write='w')
 
 
