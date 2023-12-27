8a7956d6154c6bb7b47c563d5b9f4ff3d73d2de9
jradcliffe5 <jack.radcliffe@manchester.ac.uk>
2023-12-27 11:47:46 +0300

    more logging

--- setup_sfxc/setup_sfxc.py
+++ setup_sfxc/setup_sfxc.py
@@ -180,10 +180,10 @@ if ast.literal_eval(inputs['parallelise_scans']) == True:
 		print('Building script for conversion to measurement sets')
 		for i in list(corr_files.keys()):
 			print('Scan %s .. done'%i)
-			commands.append('%s %s -o %s/%s.ms &'%(ast.literal_eval(inputs["j2ms2_exec"])," ".join(corr_files[i]),o_dir,i))
+			commands.append('%s %s -o %s/%s.ms 2>&1 | tee %s/j2ms2_%s.log &'%(ast.literal_eval(inputs["j2ms2_exec"])," ".join(corr_files[i]),o_dir,i,o_dir,i))
 		write_job(step='run_j2ms2',commands=commands,job_manager='bash',write='w')
 		print('Building script for flagging of low correlator weights')
-		commands = ['parallel -eta -j 40 %s sfxc_helperscripts/post_processing/flag_weights.py {} %.3f ::: *.ms'%(inputs['casa_exec'],ast.literal_eval(inputs['flag_threshold']))]
+		commands = ['parallel -eta -j 40 %s sfxc_helperscripts/post_processing/flag_weights.py {} %.3f ::: *.ms 2>&1 | tee %s/flag_weights.log'%(inputs['casa_exec'],ast.literal_eval(inputs['flag_threshold']),o_dir)]
 		write_job(step='run_flag_data',commands=commands,job_manager='bash',write='w')
 
 
