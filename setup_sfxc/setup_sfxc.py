d758298e8646ac8a5d29dfb65c1551ccc29e9b2f
jradcliffe5 <jack.radcliffe@manchester.ac.uk>
2023-12-27 12:36:33 +0300

    test

--- setup_sfxc/setup_sfxc.py
+++ setup_sfxc/setup_sfxc.py
@@ -188,10 +188,11 @@ if ast.literal_eval(inputs['parallelise_scans']) == True:
 				commands.append('%s %s -o %s/%s.ms 2>&1 | tee %s/logs/j2ms2_%s.log &'%(ast.literal_eval(inputs["j2ms2_exec"])," ".join(corr_files[i]),o_dir,i,o_dir,i))
 			else:
 				commands.append('%s %s -o %s/%s.ms 2>&1 | tee %s/logs/j2ms2_%s.log &'%(ast.literal_eval(inputs["j2ms2_exec"])," ".join(corr_files[i]),ms_output,i,o_dir,i))
-		for i in list(corr_files.keys()):
-			if ms_output !="":
-				commands.append('cp -rv %s/%s.ms %s &'%(ms_output,i,o_dir))
 		commands[-1] = commands[-1].split(' &')[0]
+		if ms_output !="":
+			for i in list(corr_files.keys()):
+					commands.append('cp -rv %s/%s.ms %s &'%(ms_output,i,o_dir))
+			commands[-1] = commands[-1].split(' &')[0]
 		write_job(step='run_j2ms2',commands=commands,job_manager='bash',write='w')
 		print('Building script for flagging of low correlator weights')
 		commands = ['parallel -eta -j 40 %s sfxc_helperscripts/post_processing/flag_weights.py {} %.3f ::: *.ms 2>&1 | tee %s/logs/flag_weights.log'%(inputs['casa_exec'],ast.literal_eval(inputs['flag_threshold']),o_dir)]
