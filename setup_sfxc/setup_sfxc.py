a8eb5f34c234b176a0f9063466745e66f6cc5c86
jradcliffe5 <jack.radcliffe@manchester.ac.uk>
2023-12-28 20:18:37 +0300

    test

--- setup_sfxc/setup_sfxc.py
+++ setup_sfxc/setup_sfxc.py
@@ -78,7 +78,7 @@ for i in c_names:
 			l2r_commands.append('bash %s/job_run_sfxc_%s.%s'%(cluster_params[i]["correlation_dir"],i,cluster_params[i]["cluster_specification"]["job_manager"]))
 		else:
 			raise Exception('Job manager not recognised. Software supports only SLURM, PBS Pro and bash')
-	write_job(step='run_l2r_%s'%i,commands=l2r_commands,job_manager='bash',write='w')
+		write_job(step='run_l2r_%s'%i,commands=l2r_commands,job_manager='bash',write='w')
 
 corr_files = list_correlation_outputs(scans=ss,
 									exper=exper,
